684baaf04cca7c2ef83b209a3e2cbd7661cc0230
David Lenfesty <lenfesty@ualberta.ca>
2018-08-05 12:32:01 -0600

    Finished sendCommand function.

--- sabertooth.cpp
+++ sabertooth.cpp
@@ -12,6 +12,16 @@ SaberMotor::SaberMotor(uint8_t address, uint8_t motor_num) {
     settings.motor_num = motor_num;
 }
 
+/**@brief Function for sending command over serial.
+ *
+ * @details Writes the address, then the command number, then the command value to send over software serial.
+ */
 void SaberMotor::sendCommand(uint8_t command, uint8_t value) {
+    // From the datasheet: the checksum is a 7-bit value hat is just the addition of all values
+    uint8_t checksum = (settings.address + command + value) &0b01111111;
 
+    saber_serial.write(settings.address);
+    saber_serial.write(command);
+    saber_serial.write(value);
+    saber_serial.write(checksum);
 }
