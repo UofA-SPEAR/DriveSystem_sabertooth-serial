edafd5773b173b819e535cdbef9f101d4743ed14
Jacob Reckhard <jacobrec@live.com>
2018-08-12 03:32:11 +0000

    Added some casts to make print()'s work.

--- main.cpp
+++ main.cpp
@@ -27,6 +27,8 @@ void loop() {
     if (Serial.available() >= 5) {
         if(read_serial_command(&command) == CMD_SUCCESS) {
             handle_command(&command);
+        }else{
+            while(Serial.read() != -1);
         }
     }
 
@@ -103,21 +105,27 @@ static cmd_err_t read_serial_command(cmd_t * cmd) {
     char check = buf[3];
     char end = buf[4];
 
-    Serial.print(buf[0]);
+    Serial.print((uint8_t)buf[0]);
     Serial.print(" ");
     Serial.print(buf[1]);
     Serial.print(" ");
-    Serial.print(buf[2]);
+    Serial.print((uint8_t)buf[2]);
     Serial.print(" ");
-    Serial.print(buf[3]);
+    Serial.print((uint8_t)buf[3]);
     Serial.print(" ");
-    Serial.print(buf[4]);
-    Serial.print("\n");
+    Serial.print((uint8_t)buf[4]);
+
+
 
     if(start != 2 || end != 3 || 
             check != ((uint8_t)(((uint8_t)buf[1]) + ((uint8_t)buf[2])))){
+        Serial.print(" Rejected");
+        Serial.print("\n\r");
         return CMD_ERR_INVALID;
     }
+   
+    Serial.print(" Accepted");
+    Serial.print("\n\r");
 
     time = millis();
     return CMD_SUCCESS;
